2de3a9b32af2c2e18d5d99e7b4d0d8303748cbf6
Liam Murray <122718074+lmurray045@users.noreply.github.com>
2024-11-08 13:06:03 -0800

    Alert Button added!

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-// todo
+import "./style.css";
+
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const gameName = "GeoCacher!";
+document.title = gameName;
+
+const alertButton = document.createElement("button");
+alertButton.innerHTML = "Get Alerted!";
+app.append(alertButton);
+
+alertButton.addEventListener("click", () => {
+    alert("🚨 Button Pressed!! 🚨")
+})
